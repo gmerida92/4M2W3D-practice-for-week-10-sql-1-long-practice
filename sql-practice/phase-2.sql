73413fe138aa22274d69bc2557dd72eb6985e36f
George Merida <gmerida92@gmail.com>
2022-06-08 20:23:43 -0700

    customers table

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,10 @@
 -- Your code here
+
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(40) NOT NULL,
+    phone NUMERIC(10, 0) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    reward_points INTEGER NOT NULL DEFAULT 5,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
